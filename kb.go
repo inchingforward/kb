efef66579cff311ced5b0f9c128af0f8bddf3b8f
mjanger <inchingforward@gmail.com>
2017-04-03 13:24:01 -0500

    Black triangle.

--- kb.go
+++ kb.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"github.com/oskca/gopherjs-canvas"
+	"github.com/oskca/gopherjs-dom"
 )
 
+var (
+	ctx          *canvas.Context2D
+	canvasWidth  float64
+	canvasHeight float64
+)
+
+func drawTriangle(ctx *canvas.Context2D) {
+	ctx.Save()
+	ctx.LineWidth = 1
+	ctx.StrokeStyle = "black"
+	ctx.FillStyle = "black"
+	ctx.Translate(canvasWidth/2, canvasHeight/2)
+	ctx.BeginPath()
+	ctx.MoveTo(0, 0)
+	ctx.LineTo(50, 50)
+	ctx.LineTo(-50, 50)
+	ctx.LineTo(0, 0)
+	ctx.Fill()
+	ctx.Stroke()
+	ctx.Restore()
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	window := dom.Window()
+	doc := window.Document
+	cnvs := canvas.New(doc.GetElementById("canvas").Object)
+	ctx = cnvs.GetContext2D()
+	canvasWidth = float64(cnvs.Width)
+	canvasHeight = float64(cnvs.Height)
+
+	drawTriangle(ctx)
 }
